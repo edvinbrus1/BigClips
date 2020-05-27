e5226018b56b1c98b484edc1c1742ea2948d2eb1
Edvin B <60152767+edvinbrus1@users.noreply.github.com>
2020-05-27 15:34:49 +0200

    fomatting

--- src/bigClips/Controller.java
+++ src/bigClips/Controller.java
@@ -12,12 +12,15 @@ import spaceInvaders.SpaceInvaders;
 import treIRad.Control;
 
 import javax.swing.*;
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 /**
  * Controller class for the main game, controls the various FXML documents and the launching
  * of the two smaller games that have been implemented
+ *
  * @author Edvin
  */
 public class Controller {
@@ -27,6 +30,7 @@ public class Controller {
     /**
      * Method for controlling what happens when the user clicks play on the start menu,
      * loads up the textWindow fxml document and sets the scene to that fxml document
+     *
      * @param event mouse clicked
      * @throws IOException IOException
      */
@@ -36,11 +40,9 @@ public class Controller {
 
         Scene textScene = new Scene(textView);
 
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(textScene);
         window.show();
-
-
 
 
     }
@@ -49,6 +51,7 @@ public class Controller {
      * Controlling what happens when a user presses the play button in the first text window,
      * after the user has read the first piece of the story the play button is expected to
      * be pressed which loads the firstGameWindow scene and starts the Space Invaders mini game.
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
@@ -58,33 +61,34 @@ public class Controller {
 
         Scene firstGameScene = new Scene(firstGameView);
 
-        Stage window = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.setScene(firstGameScene);
         window.show();
 
-       Platform.runLater(() -> {
-           try {
-               new SpaceInvaders().start(new Stage());
-           } catch (Exception e) {
-               e.printStackTrace();
-           }
-       });
+        Platform.runLater(() -> {
+            try {
+                new SpaceInvaders().start(new Stage());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     /**
      * Controlling what happens when the user presses the continue button on the first game window,
      * the method loads another FXML document and creates a new scene including another piece
      * of the games story
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
     @FXML
-    public void fgwContClicked(MouseEvent mouseEvent) throws IOException{
+    public void fgwContClicked(MouseEvent mouseEvent) throws IOException {
         Parent secondTextWindow = FXMLLoader.load(getClass().getResource("secondTextWindow.fxml"));
 
         Scene secondTextWindowScene = new Scene(secondTextWindow);
 
-        Stage window = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.setScene(secondTextWindowScene);
         window.show();
     }
@@ -94,16 +98,17 @@ public class Controller {
      * after the second piece of the story has been read the user presses the play button which
      * loads up the secondGameWindow fxml document and creates a scene. It also launches the Tre
      * i rad mini game.
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
     @FXML
-    public void secondTWPClicked(MouseEvent mouseEvent) throws IOException{
+    public void secondTWPClicked(MouseEvent mouseEvent) throws IOException {
         Parent secondGameWindow = FXMLLoader.load(getClass().getResource("secondGameWindow.fxml"));
 
         Scene secondGameWindowScene = new Scene(secondGameWindow);
 
-        Stage window = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.setScene(secondGameWindowScene);
         window.show();
 
@@ -116,16 +121,17 @@ public class Controller {
      * Controlling what happens when the user presses the continue button in the second game window,
      * Since we have only managed to implement two smaller games at this moment, the player will be
      * brought to the result window in which the user can see the total score of the two games.
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
     @FXML
-    public void sgwContClicked(MouseEvent mouseEvent) throws IOException{
+    public void sgwContClicked(MouseEvent mouseEvent) throws IOException {
         Parent resultWindow = FXMLLoader.load(getClass().getResource("resultWindow.fxml"));
 
         Scene resultWindowScene = new Scene(resultWindow);
 
-        Stage window = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.setScene(resultWindowScene);
         window.show();
 
@@ -142,34 +148,36 @@ public class Controller {
 
         // behövs kanske inte o visa alla poängen separat också men gjorde det för o se att allt funkade
 
-        JOptionPane.showMessageDialog(null,"total: " + totalScore +"\nTre i rad: " + TreScore
-        + "\nSpace invader: "+ SpaceScore);     // för o testa poängsystem
+        JOptionPane.showMessageDialog(null, "total: " + totalScore + "\nTre i rad: " + TreScore
+                + "\nSpace invader: " + SpaceScore);     // för o testa poängsystem
 
     }
 
     /**
      * Method for controlling what happens when the Exit game button is clicked
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
     @FXML
-    public void gameExitClicked(MouseEvent mouseEvent) throws IOException{
+    public void gameExitClicked(MouseEvent mouseEvent) throws IOException {
         Platform.exit();
     }
 
     /**
      * Controlling what happens when the user presses return within the settings window,
      * when the user presses return they will be brought back to the start menu
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
     @FXML
-    public void settingsReturnClicked(MouseEvent mouseEvent) throws IOException{
+    public void settingsReturnClicked(MouseEvent mouseEvent) throws IOException {
         Parent startMenu = FXMLLoader.load(getClass().getResource("startMenu.fxml"));
 
         Scene startMenuScene = new Scene(startMenu);
 
-        Stage window = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.setScene(startMenuScene);
         window.show();
     }
@@ -177,22 +185,24 @@ public class Controller {
     /**
      * Controlling what happens when the settings shape is clicked in the startMenu, which is that
      * they will be taken to the settings scene.
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
     @FXML
-    public void settingsClicked(MouseEvent mouseEvent) throws IOException{
+    public void settingsClicked(MouseEvent mouseEvent) throws IOException {
         Parent settings = FXMLLoader.load(getClass().getResource("settingsScene.fxml"));
 
         Scene settingsScene = new Scene(settings);
 
-        Stage window = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.setScene(settingsScene);
         window.show();
     }
 
     /**
      * Method for exiting the game when the user presses exit on the start menu
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
@@ -203,6 +213,7 @@ public class Controller {
 
     /**
      * Method for allowing the user to try again on the Space Invaders mini game
+     *
      * @param mouseEvent mouse clicked
      * @throws IOException IOException
      */
@@ -210,8 +221,6 @@ public class Controller {
     public void fgwTryAgainClicked(MouseEvent mouseEvent) throws IOException {
         textWindowPlayClicked(mouseEvent);
     }
-
-
 
 
 }
