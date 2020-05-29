761541c7032b8ee1fab1868539572e3e20a0b6c1
Edvin B <60152767+edvinbrus1@users.noreply.github.com>
2020-05-29 16:07:38 +0200

    JavaDoc commented Launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,3 +1,7 @@
+/**
+ * Launcher class that is used for the applications JAR File, simply repeats
+ * the StartGame main method without extending Application.
+ */
 public class Launcher {
     public static void main(String[]args){
         bigClips.StartGame.main(args);
