32dc1be382a70cbbb04bd11efad1e38f86118e27
Edvin B <60152767+edvinbrus1@users.noreply.github.com>
2020-05-29 16:12:58 +0200

    Tracing

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,7 @@
 /**
  * Launcher class that is used for the applications JAR File, simply repeats
  * the StartGame main method without extending Application.
+ * @author Edvin
  */
 public class Launcher {
     public static void main(String[]args){
